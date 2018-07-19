3743b99397d546b374ba29415cfb0ea20dbb2e32
Emily Jirles <emily.jirles@gmail.com>
2018-07-19 15:43:19 +0000

    automatically backed up by learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,11 +5,7 @@ class CustomersController < ApplicationController
   end
 
   def upload
-<<<<<<< HEAD
     LeadsWorker.perform_async(params[:leads].path)
-=======
-    LeadsWorker.perform_async(params[:leads].pathredis)
->>>>>>> 7819c3f3b2da452e6db1e50536104ee668072b96
     redirect_to customers_path
   end
 
